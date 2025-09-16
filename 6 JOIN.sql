5d76203ab3cf06eb6f302a2be8fc30dbc54f3ba1
omarseddati16 <omarseddati16@gmail.com>
2025-09-16 17:05:58 +0200

    fix

--- 6 JOIN.sql
+++ 6 JOIN.sql
@@ -9,5 +9,5 @@ JOIN `course_teacher` ON `course_teacher`.`teacher_id` =  `teachers`.`id`
 JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id`
 JOIN `degrees` ON `courses`.`degree_id` = `degrees`.`id`
 JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
-WHERE `departments`.`id` = 5
+WHERE `departments`.`name` LIKE '%Matematica%';
 
